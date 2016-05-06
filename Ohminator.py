df86e845c86854977429dd61c353b134da09ed68
Rune Johan Borgli <rune.borgli@gmail.com>
2016-05-06 13:15:33 +0200

    Added more intros and error checking

--- Ohminator.py
+++ Ohminator.py
@@ -5,7 +5,8 @@ import asyncio
 class Ohminator:
     current_voice = None
     active_player = None
-    intro_names = {'runarl', 'Rune', 'Chimeric', 'Johngel'}
+    intro_names = {'runarl', 'Rune', 'Chimeric', 'Johngel', 'Christian Berseth', 'Jan Ivar Ugelstad', 'Christian F. Vegard'}
+    black_list = {'Christian Berseth'}
 
 class Stats:
     time_spent_total = None
@@ -39,6 +40,11 @@ async def on_message(message):
     """
     if message.content.startswith('!yt'):
         link = message.content[4:]
+        if message.author.voice_channel is None:
+            await client.delete_message(message)
+            await client.send_message(message.channel, '{}: Please join a voice channel to play your link!'.format(message.author.name))
+            return
+
         if ohm.current_voice is None:
             ohm.current_voice = await client.join_voice_channel(message.author.voice_channel)
         elif ohm.current_voice.channel != message.author.voice_channel:
@@ -69,7 +75,8 @@ async def on_message(message):
                                   'List of commands: \n'
                                   '!help or !command\t-\tPrints out this menu.\n'
                                   '!yt [youtube link]\t\t-\tPlays audio from YouTube in voice channel of caller.\n'
-                                  '!stahp\t\t\t\t\t\t-\tStops any playing sound.\n')
+                                  '!stahp\t\t\t\t\t\t-\tStops any playing sound.\n'
+                                  '!joined\t\t\t\t\t-\tTells you when you joined Ohm!\n')
 
     elif message.content.startswith('!ohminator'):
         await client.delete_message(message)
@@ -79,12 +86,22 @@ async def on_message(message):
     elif message.content.startswith('!roll'):
         pass
 
+    elif message.content.startswith('!joined'):
+        await client.delete_message(message)
+        await client.send_message(message.channel, '{}: You joined the Ohm server {}!'.format(message.author.name, message.author.joined_at))
+
 
 
 @client.event
 async def on_voice_state_update(before, after):
+    if after.voice_channel is None:
+        return
     if before.name is not None and before.name in ohm.intro_names \
             and before.voice_channel != after.voice_channel:
+        if after.voice_channel.name != 'Supreme Ohhhhhmmmmmmm':
+            for member in after.voice_channel.voice_members:
+                if member.name in ohm.black_list:
+                    return
 
         if ohm.current_voice is None:
             ohm.current_voice = await client.join_voice_channel(after.voice_channel)
@@ -107,8 +124,6 @@ async def on_voice_state_update(before, after):
                 """
             else:
                 return
-        else:
-            return
 
         if ohm.active_player is not None and ohm.active_player.is_playing():
             ohm.active_player.stop()
@@ -119,9 +134,15 @@ async def on_voice_state_update(before, after):
             elif before.name == 'Johngel':
                 ohm.active_player = ohm.current_voice.create_ffmpeg_player('Christer.wav')
             elif before.name == 'Chimeric':
-                ohm.active_player = ohm.current_voice.create_ffmpeg_player('MariusJingle.wav')
+                ohm.active_player = ohm.current_voice.create_ffmpeg_player('MarijusJingjeee.wav')
+            elif before.name == 'Christian Berseth':
+                ohm.active_player = ohm.current_voice.create_ffmpeg_player('last_one_niggu.wav')
+            elif before.name == 'Jan Ivar Ugelstad':
+                ohm.active_player = ohm.current_voice.create_ffmpeg_player('discord_russian_connection.wav')
+            elif before.name == 'Christian F. Vegard':
+                ohm.active_player = ohm.current_voice.create_ffmpeg_player('ChristianVJingle.wav')
             else:
-                ohm.active_player = ohm.current_voice.create_ffmpeg_player('Runejingle2.wav')
+                ohm.active_player = ohm.current_voice.create_ffmpeg_player('Bee_Gees_walk.wav')
             ohm.active_player.start()
         except Exception as e:
             print(e)

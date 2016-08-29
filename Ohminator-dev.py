9c57b29b8d6a306e25a44c4a8aa1246238634b5e
Rune Johan Borgli <rune.borgli@gmail.com>
2016-08-29 12:31:08 +0200

    Now can play and list other member's intros as well as choose which intro to play

--- Ohminator-dev.py
+++ Ohminator-dev.py
@@ -23,12 +23,16 @@ class Ohminator:
 
     help_message = '{}: List of commands: \n' \
                    '**!help** or **!command**\t\t\t-\tPrints out this menu.\n' \
-                   '**!yt** *[youtube link]*\t\t\t-\t  Plays audio from YouTube in voice channel of caller.\n' \
-                   '**!stahp**\t\t\t\t\t\t\t\t  -\tStops any playing sound.\n' \
+                   '**!yt** *[youtube link]*\t\t\t-\t  Plays audio from YouTube or other sources in voice channel of caller.\n' \
+                   '**!stop**\t\t\t\t\t\t\t\t  -\tStops any playing sound.\n' \
                    '**!joined**\t\t\t\t\t\t\t\t -\tTells you when you joined Ohm!\n' \
                    '**!roll** *[lowest]* *[highest]*\t-\tRoll a number between lowest and highest.\n' \
-                   '**!intro**\t\t\t\t\t\t\t\t\t-\tPlays your intro.\n' \
                    '**!ask** *[sentence]*\t\t\t\t-\t  Use this command to talk to Ohminator.\n' \
+                   '**!intro** *[(index)]*\t\t\t\t\t\t\t\t\t-\tPlays one of your intros at random. Can play intro at given index.\n' \
+                   '**!introof** *[name of member]* *[(index)]*\t\t\t\t\t\t\t\t\t-\tPlays one intro at random of the given member. Can play intro at given index.\n' \
+                   '**!myintros**\t\t\t\t\t\t\t\t\t-\tLists all of your intros.\n' \
+                   '**!introlist** or **!listintros** *[name of member]*\t\t\t\t\t\t\t\t\t-\tLists all the intros of the given member.\n' \
+                   '**!deleteintro** *[index]*\t\t\t\t\t\t\t\t\t-\tDeletes intro at given index.\n' \
                    '**!upload** *[url]*\t\t\t\-\tUploads a intro. File must be .wav and must be downloaded from Dropbox.\n' \
                    'The url must end on .wav. Example: https://www.dropbox.com/s/znt5tt3xe9vl8su/kekkk.wav. ' \
                    'Ending with ?dl=0 is not acceptable.'
@@ -155,6 +159,31 @@ async def on_message(message):
             await client.send_message(ohm.bot_spam, '{} stopped the player!'.format(message.author.name))
             ohm.active_player.stop()
 
+    elif message.content.startswith('!introlist') or message.content.startswith('!listintros'):
+        await client.delete_message(message)
+        name_of_member = message.content[12:]
+        if name_of_member is "":
+            await client.send_message(ohm.bot_spam,
+                                      '{}: Please provide a member!'.format(
+                                          message.author.name))
+            return
+        try:
+            intro_list = os.listdir('{}/intros'.format(name_of_member))
+        except:
+            await client.send_message(ohm.bot_spam,
+                                      '{}: Member "{}" does not have access to intros!'.format(
+                                          message.author.name, name_of_member))
+            return
+
+        intro_print = str()
+        index_cnt = 1
+        for intro in intro_list:
+            intro_print += '\n**[{}]**:\t{}'.format(index_cnt, intro)
+            index_cnt += 1
+        await client.send_message(ohm.bot_spam,
+                                  '{}: Here is the intro list of "{}":{}'.format(message.author.mention, name_of_member,
+                                                                                 intro_print))
+
     elif message.content.startswith('!intro'):
         await client.delete_message(message)
         if message.author.voice_channel is None or message.author.is_afk:
@@ -181,10 +210,78 @@ async def on_message(message):
                 ohm.active_player.stop()
 
             try:
-                intro_list = os.listdir('{}/intros'.format(message.author.name))
-                ohm.active_player = voice_client.create_ffmpeg_player(
-                    '{}/intros/{}'.format(message.author.name, random.choice(intro_list)), after=ohm.after_intro)
+                if message.content.startswith('!introof'):
+                    match = re.match(r'!introof ([a-zA-Z\s]+)(\d*)', message.content)
+                    if match is None:
+                        await client.send_message(ohm.bot_spam,
+                                                  '{}: USAGE: !introof [name of member] [(index of intro)]!'.format(
+                                                      message.author.name))
+                        return
+                    name_of_member = match.group(1).strip()
+                    if name_of_member is "":
+                        await client.send_message(ohm.bot_spam,
+                                                  '{}: Please provide a member!'.format(
+                                                      message.author.name))
+                        return
+
+                    try:
+                        intro_list = os.listdir('{}/intros'.format(name_of_member))
+                    except:
+                        await client.send_message(ohm.bot_spam,
+                                                  '{}: Member "{}" does not have access to intros!'.format(
+                                                      message.author.name, name_of_member))
+                        raise NameError
+
+                    if len(match.groups()) > 2:
+                        try:
+                            given_index = int(match.group(2))
+                            if given_index < 1:
+                                # Because lists in python interprets negative indices as positive ones
+                                # I give the intro index a high number to trigger the IndexError.
+                                intro_index = 256
+                            else:
+                                intro_index = given_index-1
+                        except ValueError:
+                            intro_index = intro_list.index(random.choice(intro_list))
+                    else:
+                        intro_index = intro_list.index(random.choice(intro_list))
+
+                    try:
+                        ohm.active_player = voice_client.create_ffmpeg_player(
+                            '{}/intros/{}'.format(name_of_member, intro_list[intro_index]), after=ohm.after_intro)
+                    except:
+                        await client.send_message(ohm.bot_spam,
+                                                  '{}: Member "{}" does not have an intro!'.format(
+                                                      message.author.name, name_of_member))
+                        raise NameError
+
+                else:
+                    intro_list = os.listdir('{}/intros'.format(message.author.name))
+                    try:
+                        given_index = int(message.content[6:])
+                        if given_index < 1:
+                            # Because lists in python interprets negative indices as positive ones
+                            # I give the intro index a high number to trigger the IndexError.
+                            intro_index = 256
+                        else:
+                            intro_index = given_index-1
+                    except ValueError:
+                        intro_index = intro_list.index(random.choice(intro_list))
+
+                    try:
+                        ohm.active_player = voice_client.create_ffmpeg_player(
+                            '{}/intros/{}'.format(message.author.name, intro_list[intro_index]), after=ohm.after_intro)
+                    except IndexError:
+                        await client.send_message(ohm.bot_spam,
+                                                  '{}: The given index of {} is out of bounds!'.format(
+                                                      message.author.name, given_index))
+                        raise IndexError
+
                 ohm.active_player.start()
+            except IndexError:
+                pass
+            except NameError:
+                pass
             except Exception as e:
                 print(e)
                 await client.send_message(ohm.bot_spam,

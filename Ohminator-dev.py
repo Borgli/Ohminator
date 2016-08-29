d4f827a4164adf72541ad004f9044132c4ccaa80
Rune Johan Borgli <rune.borgli@gmail.com>
2016-08-29 08:56:37 +0200

    Bot writes to dedicated channel and after-functions made for intros and yt-dl

--- Ohminator-dev.py
+++ Ohminator-dev.py
@@ -35,6 +35,19 @@ class Ohminator:
 
     split_list = list()
 
+    bot_spam = None
+
+    clear_now_playing = asyncio.Event()
+
+    def after_yt(self):
+        client.loop.call_soon_threadsafe(self.clear_now_playing.set)
+
+        #yield from client.change_status(discord.Game())
+        print("YoutTube-video finished playing.")
+
+    def after_intro(self):
+        print("Intro finished playing.")
+
     def print_queue(self):
         print_line = str()
         first = True
@@ -49,6 +62,12 @@ class Ohminator:
 ohm = Ohminator()
 cb = cleverbot.Cleverbot()
 
+async def should_clear_now_playing():
+    while not client.is_closed:
+        await ohm.clear_now_playing.wait()
+        await client.change_status(discord.Game())
+        ohm.clear_now_playing.clear()
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -57,23 +76,24 @@ async def on_ready():
     print(discord.version_info)
     print(discord.__version__)
     print('------')
+    ohm.bot_spam = discord.utils.find(lambda c: c.name == 'bot-spam', client.get_all_channels())
 
 @client.event
 async def on_message(message):
     if message.content.startswith('ping'):
-        await client.send_message(message.channel, 'Pong!')
+        await client.send_message(ohm.bot_spam, 'Pong!')
 
     elif message.content.startswith('!yt'):
         link = message.content[4:]
         await client.delete_message(message)
 
         if message.author.voice_channel is None or message.author.is_afk:
-            await client.send_message(message.channel,
+            await client.send_message(ohm.bot_spam,
                                       '{}: Please join a voice channel to play your link!'.format(message.author.name))
             return
 
         if ohm.active_player is not None and ohm.active_player.is_playing():
-            await client.send_message(message.channel,
+            await client.send_message(ohm.bot_spam,
                                       '{}: Something is already playing!'.format(message.author.name))
             return
 
@@ -87,7 +107,7 @@ async def on_message(message):
             elif voice_client.channel != message.author.voice_channel:
                 await voice_client.move_to(message.author.voice_channel)
         except:
-            await client.send_message(message.channel,
+            await client.send_message(ohm.bot_spam,
                                       '{}: Could not connect to voice channel!'.format(message.author.name))
             return
 
@@ -117,21 +137,22 @@ async def on_message(message):
             option = None
 
         try:
-            ohm.active_player = await voice_client.create_ytdl_player(link, options=option)
-            await client.send_message(message.channel,
+            ohm.active_player = await voice_client.create_ytdl_player(link, options=option, after=ohm.after_yt)
+            await client.change_status(discord.Game(name=ohm.active_player.title))
+            await client.send_message(ohm.bot_spam,
                                       '{}: \nNow playing: {}\nIt is {} seconds long'.format(message.author.name,
                                                                                             ohm.active_player.title,
                                                                                             ohm.active_player.duration))
             ohm.active_player.start()
         except:
-            await client.send_message(message.channel, '{}: Your link could not be played!'.format(message.author.name))
+            await client.send_message(ohm.bot_spam, '{}: Your link could not be played!'.format(message.author.name))
 
     elif message.content.startswith('!stahp') or message.content.startswith('!stop'):
         await client.delete_message(message)
         if ohm.active_player is None or not ohm.active_player.is_playing:
-            await client.send_message(message.channel, '{}: No active player!'.format(message.author.name))
+            await client.send_message(ohm.bot_spam, '{}: No active player!'.format(message.author.name))
         else:
-            await client.send_message(message.channel, '{} stopped the player!'.format(message.author.name))
+            await client.send_message(ohm.bot_spam, '{} stopped the player!'.format(message.author.name))
             ohm.active_player.stop()
 
     elif message.content.startswith('!intro'):
@@ -152,7 +173,7 @@ async def on_message(message):
                     await voice_client.move_to(message.author.voice_channel)
             except Exception as e:
                 print(e)
-                await client.send_message(message.channel,
+                await client.send_message(ohm.bot_spam,
                                           '{}: Could not connect to voice channel!'.format(message.author.name))
                 return
 
@@ -162,17 +183,17 @@ async def on_message(message):
             try:
                 intro_list = os.listdir('{}/intros'.format(message.author.name))
                 ohm.active_player = voice_client.create_ffmpeg_player(
-                    '{}/intros/{}'.format(message.author.name, random.choice(intro_list)))
+                    '{}/intros/{}'.format(message.author.name, random.choice(intro_list)), after=ohm.after_intro)
                 ohm.active_player.start()
             except Exception as e:
                 print(e)
-                await client.send_message(message.channel,
+                await client.send_message(ohm.bot_spam,
                                           '{}: Could not play intro!'.format(message.author.name))
             return
 
     elif message.content.startswith('!member'):
         await client.delete_message(message)
-        await client.send_message(message.channel,
+        await client.send_message(ohm.bot_spam,
                                   'Not a member yet?'
                                   '\nBecome a member today for a limited time offer of 5$/month.'
                                   '\nEnjoy perks as unlimited YouTube plays, '
@@ -180,60 +201,60 @@ async def on_message(message):
 
     elif message.content.startswith('!spam'):
         await client.delete_message(message)
-        await client.send_message(message.channel,
+        await client.send_message(ohm.bot_spam,
                                   '{}: Please pay 5$ to unlock the !spam command.'.format(message.author.name))
 
     elif message.content.startswith('!ohmage'):
         await client.delete_message(message)
-        await client.send_message(message.channel, '{}: Ohm was created {}!'.format(message.author.name,
+        await client.send_message(ohm.bot_spam, '{}: Ohm was created {}!'.format(message.author.name,
                                                                                     message.server.created_at))
 
     elif message.content.startswith('fuck'):
-        await client.send_message(message.channel, 'No, fuck you, {}!'.format(message.author.name))
+        await client.send_message(ohm.bot_spam, 'No, fuck you, {}!'.format(message.author.name))
 
     elif message.content.startswith('!help') or message.content.startswith('!commands'):
         await client.delete_message(message)
-        await client.send_message(message.channel, ohm.help_message.format(message.author.name))
+        await client.send_message(ohm.bot_spam, ohm.help_message.format(message.author.name))
 
     elif message.content.startswith('!ohminator'):
         await client.delete_message(message)
-        await client.send_message(message.channel, 'I am the Ohminator, bleep, bloop! Type !help to see a list of '
+        await client.send_message(ohm.bot_spam, 'I am the Ohminator, bleep, bloop! Type !help to see a list of '
                                                    'commands!')
 
     elif message.content.startswith('!roll'):
         try:
             options = message.content.split()
             rand = random.randint(int(options[1]), int(options[2]))
-            await client.send_message(message.channel, '{}: You rolled {}!'.format(message.author.name, rand))
+            await client.send_message(ohm.bot_spam, '{}: You rolled {}!'.format(message.author.name, rand))
         except:
-            await client.send_message(message.channel,
+            await client.send_message(ohm.bot_spam,
                                       '{}: Could not roll with those parameters!'.format(message.author.name))
 
     elif message.content.startswith('!joined'):
         await client.delete_message(message)
-        await client.send_message(message.channel, '{}: You joined the Ohm server {}!'.format(message.author.name,
+        await client.send_message(ohm.bot_spam, '{}: You joined the Ohm server {}!'.format(message.author.name,
                                                                                               message.author.joined_at))
 
     elif message.content.startswith('!ask'):
         question = message.content[5:]
         try:
-            await client.send_message(message.channel, cb.ask(question))
+            await client.send_message(ohm.bot_spam, cb.ask(question))
         except Exception as e:
             print(e)
-            await client.send_message(message.channel, '{}: That is not a question I will answer!'.format(message.author.name))
+            await client.send_message(ohm.bot_spam, '{}: That is not a question I will answer!'.format(message.author.name))
 
     elif message.content.startswith('!join'):
         await client.delete_message(message)
         if message.author not in ohm.split_list:
             ohm.split_list.append(message.author)
-            await client.send_message(message.channel,
+            await client.send_message(ohm.bot_spam,
                                       '{}: You have joined the queue!\n'
                                       'Current queue: {}'.format(message.author.name, ohm.print_queue()))
 
     elif message.content.startswith('!leave'):
         await client.delete_message(message)
         ohm.split_list.remove(message.author)
-        await client.send_message(message.channel,
+        await client.send_message(ohm.bot_spam,
                                   '{}: You have left the queue!\n'
                                   'Current queue: {}'.format(message.author.name, ohm.print_queue()))
 
@@ -242,7 +263,7 @@ async def on_message(message):
         try:
             num_teams = int(message.content[7:])
         except:
-            await client.send_message(message.channel,
+            await client.send_message(ohm.bot_spam,
                                       '{}: Invalid parameter for !split. Use number of teams!'.format(message.author.name))
             return
 
@@ -285,7 +306,7 @@ async def on_message(message):
         for intro in intro_list:
             intro_print += '\n**[{}]**:\t{}'.format(index_cnt, intro)
             index_cnt += 1
-        await client.send_message(message.channel,
+        await client.send_message(ohm.bot_spam,
                                   '{}: Intro list:{}'.format(message.author.mention, intro_print))
 
     elif message.content.startswith('!deleteintro'):
@@ -293,22 +314,22 @@ async def on_message(message):
         try:
             intro_index = int(message.content[12:])
             if intro_index < 1 or intro_index > 5:
-                await client.send_message(message.channel,
+                await client.send_message(ohm.bot_spam,
                                           '{}: Index is out of bounds!'.format(message.author.name))
                 return
         except:
-            await client.send_message(message.channel,
+            await client.send_message(ohm.bot_spam,
                                       '{}: Invalid parameter. Must be the index of the intro to delete!'.format(message.author.name))
             return
 
         try:
             intro_list = os.listdir('{}/intros'.format(message.author.name))
-            await client.send_message(message.channel,
+            await client.send_message(ohm.bot_spam,
                                       '{}: Deleting intro {} at index {}'.format(
                                           message.author.name, intro_list[intro_index-1], intro_index-1))
             os.remove('{}/intros/{}'.format(message.author.name, intro_list[intro_index-1]))
         except:
-            await client.send_message(message.channel,
+            await client.send_message(ohm.bot_spam,
                                       '{}: Could not remove file. No file found at given index.'.format(message.author.name))
             return
 
@@ -319,52 +340,22 @@ async def on_message(message):
             find_name = re.findall(r'\/([a-zA-z\d]+?.wav).*?$', url)
             file_name = find_name.pop()
         except:
-            await client.send_message(message.channel,'{}: Invalid file.'.format(message.author.name))
+            await client.send_message(ohm.bot_spam,'{}: Invalid file.'.format(message.author.name))
             return
         intro_list = os.listdir('{}/intros'.format(message.author.name))
         if (len(intro_list)+1) > 5:
-            await client.send_message(message.channel,
+            await client.send_message(ohm.bot_spam,
                                       '{}: You have reached the maximum number of intros. '
                                       'Please delete an intro before uploading a new one'.format(
                                           message.author.name))
             return
         url += '?dl=1&pv=1'
-        '''
-        try:
-            db_client.files_download_to_file('/{}/intros/{}'.format(message.author.name, file_name), url)
-        except Exception as e:
-            print(e)
-            await client.send_message(message.channel, '{}: Could not download file.'.format(message.author.name))
-        '''
 
         file, header = urllib.request.urlretrieve(url)
         path = os.path.realpath(file)
         os.rename(path, '{}/intros/{}'.format(message.author.name, file_name))
+        await client.send_message(ohm.bot_spam, '{}: Upload successful.'.format(message.author.name))
 
-        '''
-        fd = None
-        try:
-            find_name = re.findall(r'\/([a-zA-z\d]+?.wav).*?$', url)
-            file_name = find_name.pop()
-            u = urllib.request.urlopen(url)
-            fd = open('{}/intros/{}'.format(message.author.name, file_name), 'wb')
-
-            file_size_dl = 0
-            block_sz = 8192
-            while True:
-                buffer = u.read(block_sz)
-                if not buffer:
-                    break
-
-                file_size_dl += len(buffer)
-                fd.write(buffer)
-        except:
-            await client.send_message(message.channel,
-                                      '{}: Could not download file.'.format(message.author.name))
-        finally:
-            if fd is not None:
-                fd.close()
-        '''
 @client.event
 async def on_voice_state_update(before, after):
     if after.voice_channel is None or after.is_afk:
@@ -395,9 +386,10 @@ async def on_voice_state_update(before, after):
         try:
             intro_list = os.listdir('{}/intros'.format(after.name))
             ohm.active_player = voice_client.create_ffmpeg_player(
-                '{}/intros/{}'.format(after.name, random.choice(intro_list)))
+                '{}/intros/{}'.format(after.name, random.choice(intro_list)), after=ohm.after_intro)
             ohm.active_player.start()
         except Exception as e:
             print(e)
 
+client.loop.create_task(should_clear_now_playing())
 client.run('MTc2NDMzMTMwMzM1NTAyMzM3.CgvoFg.FLaupAZZ5OviZ1Fb7gAO_Aq-sLo')

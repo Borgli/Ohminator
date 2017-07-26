571b6c35df6fe6a712c45f2bb5d5340febed197f
Rune Johan Borgli <rune.borgli@gmail.com>
2017-07-26 17:03:47 +0200

    Ohminator.py: Changed hardcoded token into read from file

--- Ohminator.py
+++ Ohminator.py
@@ -9,5 +9,9 @@ client = discord.Client()
 # Contains all events in the event loop and all event handling
 events = Events.Events(client)
 
+# Reads token
+with open("token.txt", 'r') as f:
+    token = f.read()
+
 # Starts the execution of the bot
-client.run('MTc2NDMzMTMwMzM1NTAyMzM3.CgvoFg.FLaupAZZ5OviZ1Fb7gAO_Aq-sLo')
+client.run(token)

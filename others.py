73bc016673c4348ab28a4960da2e09a26ac0f82b
Rune Johan Borgli <rune.borgli@gmail.com>
2017-08-17 01:04:07 +0200

    others.py: Fixed bug with players that had some playlists unranked. Also added documentation

--- others.py
+++ others.py
@@ -13,13 +13,13 @@ async def get_rl_rank(message, bot_channel, client):
             embed.title = "Season {} ranks:".format(str(len(player.ranked_seasons)))
             embed.colour = discord.Colour.purple()
             latest_season = player.ranked_seasons[str(len(player.ranked_seasons))]
-            solo_duel = latest_season['10']
-            doubles = latest_season['11']
-            solo_standard = latest_season['12']
-            standard = latest_season['13']
+            solo_duel = 'No rank' if '10' not in latest_season else latest_season['10'].rankPoints
+            doubles = 'No rank' if '11' not in latest_season else latest_season['11'].rankPoints
+            solo_standard = 'No rank' if '12' not in latest_season else latest_season['12'].rankPoints
+            standard = 'No rank' if '13' not in latest_season else latest_season['13'].rankPoints
             embed.set_image(url=player.signature_url).set_thumbnail(url=player.avatar_url)\
-                .add_field(name='Solo Duel', value='{} points'.format(solo_duel.rankPoints)).add_field(name='Solo Standard', value='{} points'.format(solo_standard.rankPoints)) \
-                .add_field(name='Doubles', value='{} points'.format(doubles.rankPoints)).add_field(name='Standard', value='{} points'.format(standard.rankPoints))
+                .add_field(name='Solo Duel', value='{} points'.format(solo_duel)).add_field(name='Solo Standard', value='{} points'.format(solo_standard)) \
+                .add_field(name='Doubles', value='{} points'.format(doubles)).add_field(name='Standard', value='{} points'.format(standard))
             await client.send_message(bot_channel, "{}:".format(message.author.name), embed=embed)
         except rocket_snake.exceptions.APINotFoundError:
             await client.send_message(bot_channel, "{}: Sorry, couldn't find player!".format(message.author.name))

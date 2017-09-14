8b36ee481432c0a7db287fe550f4c4c393444085
Rune Johan Borgli <rune.borgli@gmail.com>
2017-09-14 03:00:13 +0200

    PlaylistElement.py: Added options to fix youtube urls that has expired

--- PlaylistElement.py
+++ PlaylistElement.py
@@ -31,7 +31,10 @@ class PlaylistElement:
     async def get_new_player(self):
         voice_client = self.client.voice_client_in(self.server)
         try:
-            player = await voice_client.create_ytdl_player(self.link, options=self.option, after=self.after_yt, ytdl_options={'quiet':True})
+            player = await voice_client.create_ytdl_player(self.webpage_url, options=self.option,
+                                                           before_options="-reconnect_streamed 1"
+                                                                          " -reconnect_delay_max 5",
+                                                           after=self.after_yt, ytdl_options={'quiet':True})
         except:
             return None
         self.player = player

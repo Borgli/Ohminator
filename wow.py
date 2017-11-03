13d4668f9f278b96ee6ec2861c17a9592871bf50
Rune Johan Borgli <rune.borgli@gmail.com>
2017-11-03 21:01:21 +0100

    wow.py: Use decorator for part of playername function

--- wow.py
+++ wow.py
@@ -94,62 +94,55 @@ async def playername(message, bot_channel, client):
     except:
         return
 
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(player_url, timeout=10) as resp:
-                if resp.status != 200:
-                    await client.send_message(bot_channel,
-                                              "Sorry, but something is wrong with the web site. Try again later!")
-                    return
-                character_doc = await resp.text()
+    @get_session(player_url)
+    async def create_embed(message, bot_channel, client, resp):
+        character_doc = await resp.text()
 
-                embed = discord.Embed()
-                embed.title = "Realmplayers Character Info for {}:".format(name)
-                embed.colour = discord.Colour.purple()
-                embed.url = player_url
+        embed = discord.Embed()
+        embed.title = "Realmplayers Character Info for {}:".format(name)
+        embed.colour = discord.Colour.purple()
+        embed.url = player_url
 
-                total_item_stats = re.search('Total Item Stats.+', character_doc)
-                if not total_item_stats:
-                    await client.send_message(bot_channel,
-                                              "Sorry, the web site has been changed and the bot needs an update.\n"
-                                              "Please notify the developer.")
-                    return
-                total_item_stats = total_item_stats.group(0)
+        total_item_stats = re.search('Total Item Stats.+', character_doc)
+        if not total_item_stats:
+            await client.send_message(bot_channel,
+                                      "Sorry, the web site has been changed and the bot needs an update.\n"
+                                      "Please notify the developer.")
+            return
+        total_item_stats = total_item_stats.group(0)
 
-                pattern = "\W(\w+?|\w+? \w+?|\w+? \w+? \w+?): " \
-                          "(?=<font color='#fff'>(\d*?)<\/font> \+ <.*?>(.*?)<.*?><br|<.*?>(.*?)<.*?>)"
+        pattern = "\W(\w+?|\w+? \w+?|\w+? \w+? \w+?): " \
+                  "(?=<font color='#fff'>(\d*?)<\/font> \+ <.*?>(.*?)<.*?><br|<.*?>(.*?)<.*?>)"
 
-                item_stats_list = re.findall(pattern, total_item_stats)
-                embed.description = "__**Total Item Stats:**__"
-                for item in item_stats_list:
-                    embed.description += "\n{}: **{}**".format(
-                        item[0], item[3] if item[3] != "" else "{} + {}".format(item[1], item[2]))
-                embed.description += "\n\n__**PVP Stats:**__"
+        item_stats_list = re.findall(pattern, total_item_stats)
+        embed.description = "__**Total Item Stats:**__"
+        for item in item_stats_list:
+            embed.description += "\n{}: **{}**".format(
+                item[0], item[3] if item[3] != "" else "{} + {}".format(item[1], item[2]))
+        embed.description += "\n\n__**PVP Stats:**__"
 
-                character_info = re.search('Character Info.+', character_doc)
-                if not character_info:
-                    await client.send_message(bot_channel,
-                                              "Sorry, the web site has been changed and the bot needs an update.\n"
-                                              "Please notify the developer.")
-                    return
-                character_info = character_info.group(0)
+        character_info = re.search('Character Info.+', character_doc)
+        if not character_info:
+            await client.send_message(bot_channel,
+                                      "Sorry, the web site has been changed and the bot needs an update.\n"
+                                      "Please notify the developer.")
+            return
+        character_info = character_info.group(0)
 
-                rank = re.search("\WRank: (.*?)<", character_info)
-                rank_progress = re.search("\WRank Progress: (.*?)<", character_info)
-                standing = re.search("\WStanding: (.*?)<", character_info)
-                rank_change = re.search("\WRank Change: <.*?>(.*?)<", character_info)
+        rank = re.search("\WRank: (.*?)<", character_info)
+        rank_progress = re.search("\WRank Progress: (.*?)<", character_info)
+        standing = re.search("\WStanding: (.*?)<", character_info)
+        rank_change = re.search("\WRank Change: <.*?>(.*?)<", character_info)
 
-                embed.add_field(name='Rank', value="Not Available" if not rank else rank.group(1))\
-                    .add_field(name='Rank Progress', value="Not Available" if not rank_progress else rank_progress.group(1))\
-                    .add_field(name='Standing', value="Not Available" if not standing else standing.group(1))\
-                    .add_field(name='Rank Change', value="Not Available" if not rank_change else rank_change.group(1))\
+        embed.add_field(name='Rank', value="Not Available" if not rank else rank.group(1))\
+            .add_field(name='Rank Progress', value="Not Available" if not rank_progress else rank_progress.group(1))\
+            .add_field(name='Standing', value="Not Available" if not standing else standing.group(1))\
+            .add_field(name='Rank Change', value="Not Available" if not rank_change else rank_change.group(1))\
 
-                await client.send_message(bot_channel, embed=embed)
-    except asyncio.TimeoutError:
-        await client.send_message(bot_channel, 'Sorry, web site took too long to respond. Try again later')
-    except aiohttp.ClientConnectionError:
-        await client.send_message(bot_channel, 'Sorry, the bot can not connect to the web site at this moment. '
-                                               'Try again later.')
+        await client.send_message(bot_channel, embed=embed)
+
+    await create_embed(message, bot_channel, client)
+
 
 async def get_player_url(message, bot_channel, client, name):
     @get_session("http://realmplayers.com/CharacterList.aspx?search={}".format(name))

22a6995dcf8756a439ad908c6be933a74f73b2d0
Rune Johan Borgli <rune.borgli@gmail.com>
2018-02-27 16:10:06 +0100

    ohminator.py: Bot now automatically restarts

--- ohminator.py
+++ ohminator.py
@@ -1,23 +1,40 @@
+import traceback
+import asyncio
 import discord
 import pymongo
 
-import events
+from events import Events
 
-client = discord.Client()
 
-# Reads MongoDB connection parameter as it has sensitive information
-with open('mongodb-connection-parameter.txt', 'r') as f:
-    mongodb_connection_parameter = f.read().strip()
+def main():
+    client = discord.Client()
 
-db_client = pymongo.MongoClient(mongodb_connection_parameter)
-db = db_client.Ohminator
+    # Reads MongoDB connection parameter as it has sensitive information
+    with open('mongodb-connection-parameter.txt', 'r') as f:
+        mongodb_connection_parameter = f.read().strip()
 
-# Contains all events in the event loop and all event handling
-events = events.Events(client, db)
+    db_client = pymongo.MongoClient(mongodb_connection_parameter)
+    db = db_client.Ohminator
 
-# Reads token
-with open("token.txt", 'r') as f:
-    token = f.read().strip()
+    # Contains all events in the event loop and all event handling
+    events = Events(client, db)
 
-# Starts the execution of the bot
-client.run(token)
+    # Reads token
+    with open("token.txt", 'r') as f:
+        token = f.read().strip()
+
+    # Starts the execution of the bot
+    client.run(token)
+
+
+if __name__ == '__main__':
+    while True:
+        # Will automatically restart the bot
+        # The wrapper will catch KeyboardInterrupts, so any exception that occurs means restart
+        try:
+            main()
+        except:
+            traceback.print_exc()
+            asyncio.set_event_loop(asyncio.new_event_loop())
+            continue
+        break
